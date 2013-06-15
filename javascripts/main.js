abf0108750acfc200a02ce0c303faead97aff499
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2013-06-15 16:58:18 +0200

    provide tab views for native examples

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+window.addEventListener('load', function() {
+	var currentTab = null;
+	var tabs = document.querySelectorAll('nav ul li a');
+	var tabpanels = [];
+	Array.prototype.forEach.call(tabs, function (tabLink) {
+		var panelId = tabLink.attributes.href.value.substr(1);
+		var tab = document.getElementById(panelId);
+		if (tab.attributes['aria-selected'].value === "true") {
+			currentTab = tab;
+		} else {
+			tab.style.display = 'none';
+		}
+		tabLink.addEventListener('click', function onTabNavClick() {
+			currentTab.style.display = 'none';
+			tab.style.display = 'block';
+			currentTab = tab;
+		});
+	});
+});
