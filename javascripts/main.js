4ca812f1e4c3b96d65d1554494cb4dd13a1aed39
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2013-06-21 09:36:43 +0200

    better aria tab handling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,31 @@
 window.addEventListener('load', function() {
-	var currentTab = null;
-	var tabs = document.querySelectorAll('nav ul li a');
-	var tabpanels = [];
-	Array.prototype.forEach.call(tabs, function (tabLink) {
-		var panelId = tabLink.attributes.href.value.substr(1);
-		var tab = document.getElementById(panelId);
-		if (tab.attributes['aria-selected'].value === "true") {
-			currentTab = tab;
-		} else {
-			tab.style.display = 'none';
-		}
-		tabLink.addEventListener('click', function onTabNavClick() {
-			currentTab.style.display = 'none';
-			tab.style.display = 'block';
-			currentTab = tab;
-			return false;
+	var tabLists = document.querySelectorAll('nav[role=tablist]');
+	Array.prototype.forEach.call(tabLists, function forEachTabList(tablist) {
+		var currentTab = null;
+		var tabs = tablist.getElementsByTagName('a');
+		var tabpanels = [];
+		Array.prototype.forEach.call(tabs, function (tabLink) {
+			var panelId = tabLink.attributes.href.value.substr(1);
+			var tab = document.getElementById(panelId);
+			if (tab.getAttribute('aria-selected') === "true") {
+				currentTab = tab;
+			} else {
+				tab.setAttribute('aria-hidden', 'true');
+			}
+			tabLink.addEventListener('click', function onTabNavClick(event) {
+				//currentTab.style.display = 'none';
+				//tab.style.display = 'block';
+				currentTab.setAttribute('aria-selected', 'false');
+				currentTab.setAttribute('aria-hidden', 'true');
+				tab.setAttribute('aria-selected', 'true');
+				tab.setAttribute('aria-hidden', 'false');
+				currentTab = tab;
+				if (event.stopPropagation) {
+					event.stopPropagation();
+				}
+				event.cancelBubble = true;
+				return false;
+			});
 		});
 	});
 });
